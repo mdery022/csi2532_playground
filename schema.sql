1471495f94ba849d4a97c649627c7792c6854cab
mdery022 <mdery022@uottawa.ca>
2022-02-24 11:43:36 -0500

    Created migration table

--- schema.sql
+++ schema.sql
@@ -11,3 +11,13 @@ INSERT INTO athletes (id, name, identified_gender, dob) VALUES
 (3, 'Hayden', 'm', '1996-07-24'),
 (4, 'August', 'm', '1999-09-09');
 
+
+CREATE TABLE schema_migrations (
+    migration varchar(255),
+    migrated_at time,
+    PRIMARY KEY (migration)
+);
+
+INSERT INTO schema_migrations (migration, migrated_at) VALUES
+    ('20200127173000-create-athletes.sql', '2020-01-27 17:30:00'),
+    ('20200126174000-create-migrations.sql', '2020-01-27 17:40:00'); 
